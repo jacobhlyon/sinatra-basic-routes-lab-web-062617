86858d285ee22dbde169e4dcb6558889ee833217
Jacob Harrison Lyon <jacobhlyon@Jacobs-MacBook-Air.local>
2017-07-17 13:39:40 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+	get '/name' do
+		"My name is Jacob"
+	end
+
+	get '/hometown' do 
+		"My hometown is Storm Lake, Iowa"
+	end
+
+	get '/favorite-song' do 
+		"My favorite song is something very cool"
+	end
 end
